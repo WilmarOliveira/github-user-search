5b59469f01ce4e679bc1f9ffed16296547bb0f82
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2021-03-01 20:31:01 -0300

    Creating project and add Header component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Header from './core/components/Header';
+import './app.css';
+
+const App = () => {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/core/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Header = () => {
+    return (
+        <header className="header-container" >
+            <h3 className="header-title" >Bootcamp DevSuperior</h3>
+        </header>
+    );
+}
+
+export default Header;
