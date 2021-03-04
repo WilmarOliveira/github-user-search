b546591932fe5e30cf170a2cefdb9ec390cda9c4
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2021-03-03 22:10:14 -0300

    Creating FindGitProfile Page

--- src/pages/FindGitProfile/index.tsx
+++ src/pages/FindGitProfile/index.tsx
@@ -1,6 +1,15 @@
+import Button from '../../core/components/Button';
+import './styles.css';
+
 const FindGitProfile = () => {
     return (
-        <div>Teste</div>
+        <div className="search-container" >
+            <div className="search-content" >
+                <h3 className="search-title" >Encontre um perfil Github</h3>
+                <input placeholder="Usuário Github" className="search-input" type="text" />
+                <Button linkTo="/" >Encontrar</Button>
+            </div>
+        </div>
     );
 }
 
